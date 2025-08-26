156988fe6cd20d14e1a43cbdb6d0546df0922f99
Morninghead <nopanat.aplus@gmail.com>
2025-08-26 10:44:51 +0700

    Complete: Add missing public/index.html, React build working

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Inventory Management System</h1>
+        <p>System is loading...</p>
+        <p>Ready for development!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
